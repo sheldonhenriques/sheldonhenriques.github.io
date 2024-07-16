6943d25d8891201827a2ae424fbe38b3b7f2d493
Sheldon Henriques <32040506+sheldonhenriques@users.noreply.github.com>
2024-07-16 04:27:15 -0700

    works

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,27 @@ function linkAction()
 };
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Scroll ections active link
+const sections = document.querySelectorAll('section[id]');
+
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight)
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.add('active')
+        }
+        else
+        {
+            document.querySelector('.nav__menu a[href*=' + sectionId +']').classList.remove('active')
+        }
+    })
+}
